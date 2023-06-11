ab042fb9d0715f18c968f87a6a6523cdc8a7c7dc
daviddzxy <david.dzxy@gmail.com>
2023-06-11 20:38:41 +0200

    Init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+export default App
+
+function App() {
+    return (
+        <div>
+            <h1>MicroBlog</h1>
+            <button></button>
+            <button></button>
+        </div>
+    )
+}
