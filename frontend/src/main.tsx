f6e2086ad950eec425b962f939e1bcc8d8b19e8e
daviddzxy <david.dzxy@gmail.com>
2023-06-12 23:42:20 +0200

    Add redux store

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { store } from './store.ts'
+import { Provider } from 'react-redux'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </React.StrictMode>,
 )
