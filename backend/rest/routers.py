f0b7d9eca17279b0ecb0d3dcb4a6e041c47b40fb
daviddzxy <david.dzxy@gmail.com>
2023-07-02 17:21:19 +0200

    Add detail to conflict error in sign up

--- backend/rest/routers.py
+++ backend/rest/routers.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import status
 from fastapi import Response
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 from database import SessionLocal
@@ -22,14 +23,15 @@ def get_database_session():
 @signup_router.post("/sign-up", status_code=status.HTTP_201_CREATED)
 async def signup(
     user: schemas.UserCreate,
-    response: Response,
     db_session: Session = Depends(get_database_session)
 ):
     existing_user_name = db_session.execute(
         select(models.User.user_name).where(models.User.user_name == user.user_name)
     ).first()
     if existing_user_name:
-        response.status_code = status.HTTP_409_CONFLICT
-        return
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, detail=f"User with username {user.user_name} already exists."
+        )
+
     db_session.add(models.User(user_name=user.user_name, password=user.password))
     db_session.commit()
