9b4ad9b93be9dfc83f51aab74a8cb6f87fff6e2d
daviddzxy <david.dzxy@gmail.com>
2023-08-04 17:54:19 +0200

    Add follow table

---
+++ backend/rest/migrations/versions/f29d65596923_add_follow_table.py
@@ -0,0 +1,30 @@
+"""Add follow table
+
+Revision ID: f29d65596923
+Revises: 17d6c3e44adb
+Create Date: 2023-08-01 17:18:29.043795
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'f29d65596923'
+down_revision = '17d6c3e44adb'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'follow',
+        sa.Column('follower_id', sa.Integer(), nullable=False),
+        sa.Column('followee_id', sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(['followee_id'], ['user.id'], ),
+        sa.ForeignKeyConstraint(['follower_id'], ['user.id'], ),
+        sa.PrimaryKeyConstraint('follower_id', 'followee_id'),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('follow')
--- backend/rest/models.py
+++ backend/rest/models.py
@@ -1,9 +1,18 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class Follow(Base):
+    __tablename__ = "follow"
+
+    follower_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
+    followee_id: Mapped[int] = mapped_column(ForeignKey("user.id"), primary_key=True)
+    follower = relationship('User', foreign_keys=[follower_id])
+    followee = relationship('User', foreign_keys=[followee_id])
 
 
 class User(Base):
@@ -13,6 +22,20 @@ class User(Base):
     user_name: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str] = mapped_column()
     posts: Mapped[list["Post"]] = relationship(back_populates="user")
+    following: Mapped[list["User"]] = relationship(
+        secondary="follow",
+        primaryjoin=id == Follow.follower_id,
+        secondaryjoin=id == Follow.followee_id,
+        back_populates="following",
+        viewonly=True
+    )
+    followers: Mapped[list["User"]] = relationship(
+        secondary="follow",
+        primaryjoin=id == Follow.followee_id,
+        secondaryjoin=id == Follow.follower_id,
+        back_populates="followers",
+        viewonly=True
+    )
 
 
 class Post(Base):
